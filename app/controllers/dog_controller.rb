1e91064a12cb08204910b75f76a0c91bc1b91fc6
Lucas Montanari <Lmonty2011@gmail.com>
2021-06-04 15:04:41 -0600

    new dog form up and running with validations

--- app/controllers/dog_controller.rb
+++ app/controllers/dog_controller.rb
@@ -12,7 +12,13 @@ class DogController < ApplicationController
     end
 
     def create
-
+        @dog = Dog.create(name: params["name"], breed: params["breed"], bio: params[:bio], age: params[:age])
+        if @dog.valid? 
+            redirect_to dog_index_url
+        else 
+            flash[:errors] = @dog.errors.full_messages
+            redirect_to new_dog_url
+        end
     end
 
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,6 @@
 class Dog < ApplicationRecord 
-    
+    validates_presence_of :name, message: 'A dog has to have a name!' 
+    validates :age, numericality: {only_integer: true}
+    validates_presence_of :breed, message: 'You forgot to add a breed!'
+    validates_presence_of :bio, message: 'You forgot to add a bio!'
 end
